cc3c73cdfcff108897ae7da6df131f4e6d788bfb
azbyx <azbyx@inbox.ru>
2020-10-14 21:58:08 +0300

    first

---
+++ header/custom_tuple.h
@@ -0,0 +1,84 @@
+#ifndef CUSTOM_TUPLE_H_INCLUDED
+#define CUSTOM_TUPLE_H_INCLUDED
+#include <utility>
+
+
+template<typename... Args>
+struct custom_tuple;
+
+
+namespace tmp{
+    template<size_t Is, typename Head, typename... Args>
+    struct getter
+    {
+        static decltype(auto) get(const custom_tuple<Head, Args...>& t)
+        {
+            return getter<Is - 1, Args...>::get(t);
+        }
+    };
+
+    template<typename Head, typename... Args>
+    struct getter<0, Head, Args...>
+    {
+        static decltype(auto) get(const custom_tuple<Head, Args...>& t)
+        {
+            return t.value;
+        }
+    };
+}//namespace tmp{
+
+template<size_t Is, typename Head, typename... Args>
+decltype(auto) custom_get(const custom_tuple<Head, Args...>& t)
+{
+    return tmp::getter<Is, Head, Args...>::get(t);
+}
+
+template<typename H, typename... Tail>
+struct custom_tuple<H, Tail...> : custom_tuple<Tail...>{
+
+    using value_t = H;
+
+    custom_tuple(H val, Tail... tail)
+            : custom_tuple<Tail...>(tail...), value(val){}
+
+    template<typename... Args>
+    custom_tuple(const custom_tuple<Args...>& other):
+            custom_tuple(other, std::make_index_sequence<sizeof...(Args)>{}){}
+
+    template<typename... Args>
+    void operator=(const custom_tuple<Args...>& other)
+    {
+        assign_tuple(*this, other, std::make_index_sequence<sizeof...(Args)>{});
+    }
+
+    template<typename T1, typename T2, size_t ... Indices>
+	void assign_tuple(T1& th, const T2& other, std::index_sequence<Indices...>){
+        ((custom_get<Indices>(th) = custom_get<Indices>(other)), ...);
+	}
+
+    template<typename Tuple, size_t ... Indices>
+	custom_tuple(Tuple&& other, std::index_sequence<Indices...>):
+        custom_tuple(custom_get<Indices>(std::forward<Tuple>(other))...){}
+
+public:
+    value_t value = H();
+};
+
+template<>
+struct custom_tuple<>{};
+
+
+template <typename... Args>
+auto make_custom_tuple(Args&&... args)
+{
+    return custom_tuple<std::decay_t<Args>...>(std::forward<Args>(args)...);
+}
+
+
+template <typename... Args>
+auto custom_tie(Args&... args)
+{
+    return custom_tuple<Args&...>(args...);
+}
+
+#endif // CUSTOM_TUPLE_H_INCLUDED
---
+++ sourse/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <cassert>
+#include "custom_tuple.h"
+
+
+void do_it(){
+    int vari = 111;
+    double vard = 2.2;
+    const char* varstr = "ddd";
+
+    int vari1 = 999;
+    double vard1 = 0.9999;
+    const char* varstr1 = "zzz";
+    auto tpl = make_custom_tuple(vari, vard, varstr);
+
+    auto tpltie = tpl;
+    (void) tpltie;
+    //auto tplr = make_custom_tuple(1, 2, 3);
+    std::cout << "vars: vari, vard, varstr in tpl:" << std::endl;
+    std::cout << custom_get<0>(tpl) << ' ' << custom_get<1>(tpl) << ' ' << custom_get<2>(tpl) << std::endl;
+    std::cout << "vars: vari1, vard1, varstr1 before assignment:" << std::endl;
+    std::cout << vari1 << ' ' << vard1 << ' ' << varstr1 << std::endl;
+    //
+    custom_tie(vari1, vard1, varstr1) = tpl;
+    //custom_get<0>(tpltie) = custom_get<0>(tpl), custom_get<1>(tpltie) = custom_get<1>(tpl), custom_get<2>(tpltie) = custom_get<2>(tpl);
+    std::cout << "vars: vari1, vard1, varstr1 after assignment:" << std::endl;
+    std::cout << vari1 << ' ' << vard1 << ' ' << varstr1 << std::endl;
+}
+
+
+auto getPerson() {
+    const std::string name = "Petia";
+    const std::string secondName = "Ivanoff";
+    const std::size_t age = 23;
+    const std::string department = "Sale";
+    return make_custom_tuple(name, secondName, age, department);
+}
+
+int main(int, char**) {
+
+    std::string name, secondName, department;
+    std::size_t age;
+
+    custom_tie(name, secondName, age, department) = getPerson();
+
+    assert(name == "Petia");
+    assert(secondName == "Ivanoff");
+    assert(age == 23);
+    assert(department == "Sale");
+
+    //do_it();
+
+    return 0; }
+
