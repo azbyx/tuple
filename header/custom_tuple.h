012c0cae57d4d307c55d8e0ae538c095021c0bf0
azbyx <azbyx@inbox.ru>
2020-10-28 16:52:34 +0300

    implementation

--- header/custom_tuple.h
+++ header/custom_tuple.h
@@ -2,7 +2,6 @@
 #define CUSTOM_TUPLE_H_INCLUDED
 #include <utility>
 
-
 template<typename... Args>
 struct custom_tuple;
 
@@ -11,54 +10,38 @@ namespace tmp{
     template<size_t Is, typename Head, typename... Args>
     struct getter
     {
-        static decltype(auto) get(const custom_tuple<Head, Args...>& t)
-        {
-            return getter<Is - 1, Args...>::get(t);
-        }
+        static decltype(auto) get(const custom_tuple<Head, Args...>&);
     };
 
     template<typename Head, typename... Args>
     struct getter<0, Head, Args...>
     {
-        static decltype(auto) get(const custom_tuple<Head, Args...>& t)
-        {
-            return t.value;
-        }
+        static decltype(auto) get(const custom_tuple<Head, Args...>&);
     };
-}//namespace tmp{
+}//namespace tmp
 
 template<size_t Is, typename Head, typename... Args>
-decltype(auto) custom_get(const custom_tuple<Head, Args...>& t)
-{
-    return tmp::getter<Is, Head, Args...>::get(t);
-}
+decltype(auto) custom_get(const custom_tuple<Head, Args...>& );
 
 template<typename H, typename... Tail>
 struct custom_tuple<H, Tail...> : custom_tuple<Tail...>{
 
     using value_t = H;
 
-    custom_tuple(H val, Tail... tail)
-            : custom_tuple<Tail...>(tail...), value(val){}
+    custom_tuple(H, Tail...);
 
     template<typename... Args>
-    custom_tuple(const custom_tuple<Args...>& other):
-            custom_tuple(other, std::make_index_sequence<sizeof...(Args)>{}){}
+    custom_tuple(const custom_tuple<Args...>&);
 
     template<typename... Args>
-    void operator=(const custom_tuple<Args...>& other)
-    {
-        assign_tuple(*this, other, std::make_index_sequence<sizeof...(Args)>{});
-    }
+    void operator=(const custom_tuple<Args...>&);
 
     template<typename T1, typename T2, size_t ... Indices>
-	void assign_tuple(T1& th, const T2& other, std::index_sequence<Indices...>){
-        ((custom_get<Indices>(th) = custom_get<Indices>(other)), ...);
-	}
+	void assign_tuple(T1&, const T2&, std::index_sequence<Indices...>);
 
+private:
     template<typename Tuple, size_t ... Indices>
-	custom_tuple(Tuple&& other, std::index_sequence<Indices...>):
-        custom_tuple(custom_get<Indices>(std::forward<Tuple>(other))...){}
+	custom_tuple(Tuple&&, std::index_sequence<Indices...>);
 
 public:
     value_t value = H();
